0990be4972f899dea076b6659da3f140e52b2b78
csps108 <91589458+csps108@users.noreply.github.com>
2021-12-31 10:46:09 +0800

    Update autoghidra.py

--- autoghidra.py
+++ autoghidra.py
@@ -14,12 +14,12 @@ import logging
 import configparser
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fileDir='/Users/yilnhsieh/csti-malware-classify/20211008/upload/'
 def exe2asm():
     #執行Headless Analyzer指令
-    cmd =['./analyzeHeadless','/Users/yilnhsieh/csti-malware-classify/20211008/upload','test1104','-postScript','/Users/yilnhsieh/csti-malware-classify/20211008/upload/ghidra2asm.py','/Users/yilnhsieh/csti-malware-classify/20211008/upload/txt/','-import','/Users/yilnhsieh/csti-malware-classify/20211008/upload/danger/*.danger','-deleteProject']
+    cmd =['./analyzeHeadless','/Users/upload','test1104','-postScript','/Users/upload/ghidra2asm.py','/Users/upload/txt/','-import','/Users/upload/danger/*.danger','-deleteProject']
+    #cmd =['./analyzeHeadless','/path/專案存取位置','專案名稱','-postScript','/腳本位置/ghidra2asm.py','/path/腳本轉換後的資料路徑/','-import','/path/欲處理資料路徑/*.danger','-deleteProject']
     #指定到Ghidra路徑
-    os.chdir('/Users/yilnhsieh/csti-malware-classify/ghidra_10.0.4_PUBLIC/support/')
+    os.chdir('/Users/ghidra_10.0.4_PUBLIC/support/')
     print(os.getcwd())
     subprocess.Popen(cmd)
 
